f249f9be320d85039245f47a1e6224d44f4812c5
SaraHussein4 <saraomran12002@gmail.com>
2025-03-06 19:29:43 +0200

    sara

--- obj/Debug/net9.0-windows/LibraryManagementSystem.AssemblyInfo.cs
+++ obj/Debug/net9.0-windows/LibraryManagementSystem.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LibraryManagementSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f4b30bc2b8aed8096926a7fbfa5fd506ac8a82fc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0d4149da20bdb8d13b82dfd82f3b783fb827e4d7")]
 [assembly: System.Reflection.AssemblyProductAttribute("LibraryManagementSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LibraryManagementSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
