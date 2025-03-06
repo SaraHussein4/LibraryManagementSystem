5d3527196413cc0a2ac6a24e34bc4c6cc12adc74
SaraHussein4 <saraomran12002@gmail.com>
2025-03-06 16:52:25 +0200

    add loginPage

--- obj/Debug/net9.0-windows/LibraryManagementSystem.AssemblyInfo.cs
+++ obj/Debug/net9.0-windows/LibraryManagementSystem.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LibraryManagementSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b47d754d20f6bee7cc0667a6ab3480faccfeabe4")]
 [assembly: System.Reflection.AssemblyProductAttribute("LibraryManagementSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LibraryManagementSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
