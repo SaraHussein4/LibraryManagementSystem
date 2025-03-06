ed6727f902e78ee45f7860af8bfc43fca02a92a2
dua74 <helaldua74@outlook.com>
2025-03-06 17:11:48 +0200

    conflictResolved

--- obj/Debug/net9.0-windows/LibraryManagementSystem.AssemblyInfo.cs
+++ obj/Debug/net9.0-windows/LibraryManagementSystem.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LibraryManagementSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e1a53eb30b6fb7c2b4ada820e6d97836efc51807")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+097175e9579ffe97d35bfd4ee5bf90a3af568173")]
 [assembly: System.Reflection.AssemblyProductAttribute("LibraryManagementSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LibraryManagementSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
