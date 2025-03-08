f2586f7690ace5998e54c80fd3f820fdd54afa16
dua74 <helaldua74@outlook.com>
2025-03-08 22:32:10 +0200

    dua

--- obj/Debug/net9.0-windows/LibraryManagementSystem.AssemblyInfo.cs
+++ obj/Debug/net9.0-windows/LibraryManagementSystem.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LibraryManagementSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b0c2908cec6bf5c03af41ed23756f6e8033e82e4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3e3f774f559619d643f1dd349b80a276dbecced2")]
 [assembly: System.Reflection.AssemblyProductAttribute("LibraryManagementSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LibraryManagementSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
