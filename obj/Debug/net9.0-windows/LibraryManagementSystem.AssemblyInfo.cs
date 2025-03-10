572e1cdbd3ba525e92db7f8d1c941fa2a5ab08ca
dua74 <helaldua74@outlook.com>
2025-03-10 02:03:40 +0200

    pull admin form

--- obj/Debug/net9.0-windows/LibraryManagementSystem.AssemblyInfo.cs
+++ obj/Debug/net9.0-windows/LibraryManagementSystem.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LibraryManagementSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ca107ea1b21891b24fc4dc618a5ff9c3cc5ec2b2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5500cf50af8359ddea219f6c1dc0865e1a0735a4")]
 [assembly: System.Reflection.AssemblyProductAttribute("LibraryManagementSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LibraryManagementSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
